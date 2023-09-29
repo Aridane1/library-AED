2737831ecb698bf276da1fab244337d2774f289d
Davidps97 <davidperezsantana1997@gmail.com>
2023-09-29 16:04:13 +0100

    desing css

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "test1234",
+  password: "",
   database: "db_library",
 });
 
